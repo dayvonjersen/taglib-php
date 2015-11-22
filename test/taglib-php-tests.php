d58b17797f884d089e624df24e3d1b04d59e669a
tso <tso@teknik.io>
2015-11-22 00:24:38 -0500

    found some bugs with this test case already! ._.;;

--- test/taglib-php-tests.php
+++ test/taglib-php-tests.php
@@ -1,10 +1,13 @@
 <?php
+$pass_counter = $fail_counter = 0;
 assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_WARNING, 0);
-assert_options(ASSERT_BAIL, 1);
+assert_options(ASSERT_BAIL, 0);
 assert_options(ASSERT_QUIET_EVAL, 1);
 assert_options(ASSERT_CALLBACK, function($file,$line,$code,$desc="") {
-    echo "\033[31mFAIL\033[0m\n\nAssert failed: ",basename($file), ":$line\n$desc\n";
+    global $fail_counter;
+    $fail_counter++;
+    echo "\n---\n\033[31mFAIL:\033[0m",basename($file), ":$line\n$code\n$desc\n---\n";
 });
 
 assert(extension_loaded('taglib'), 'TagLib extension not loaded!');
@@ -38,20 +41,33 @@ $JSON = json_decode(file_get_contents('taglib-php-tests.json'),1);
         ],//...
     },//...
 */
+$num_tests = 0;
 foreach($JSON as $class => $methods) {
     foreach($methods as $method => $tests) {
         $testFn = "Test__{$class}__{$method}";
         if(!function_exists($testFn)) {
             echo "\033[33mWarning: no test coverage for $class::$method!\033[0m\n";
         } else {
-            echo "Running tests for $class::$method";
+            $count = count($tests);
+            $num_tests += $count;
+            $preprefail = $fail_counter;
+            echo "Running $count tests for $class::$method";
             foreach($tests as $test) {
                 $args = $test['params'];
                 $args[] = $test['result'];
+                $prefail = $fail_counter;
                 call_user_func_array($testFn,$args);
-                echo ".";
+                if($prefail === $fail_counter) {
+                    $pass_counter++;
+                    if($prefail === $preprefail) {
+                        echo ".";
+                    }
+                }
             }
-            echo "\033[32mOK\033[0m\n";
+            if($preprefail === $fail_counter) {
+                echo "\033[32mOK\033[0m\n";
+            }
         }
     }
 }
+echo "\n\n$num_tests tests\n$pass_counter PASS\n\n$fail_counter FAIL(ed assertions)\n";
---
+++ test/testcases/TagLibMPEG__setID3v1.phpt
@@ -0,0 +1,35 @@
+<?php
+function Test__TagLibMPEG__setID3v1($file) {
+    $tmpfile = "./tmp/".basename($file);
+    assert(copy($file, $tmpfile), "Couldn't copy $file to $tmpfile!");
+
+    // testing: overwrite original tags = true
+    $t = new TagLibMPEG($tmpfile);
+    $original_tags = $t->getID3v1();
+    if($original_tags === false) {
+        $original_tags = [];
+    }
+    $original_tags['TITLE'] = 'testing';
+    $t->setID3v1(['TITLE'=>'testing'], true);
+    $new_tags = $t->getID3v1();
+    assert($new_tags !== false, "No tags found in file after setID3v1 w/ overwrite original tags = TRUE!");
+    foreach($new_tags as $frameID => $value) {
+        assert($original_tags[$frameID] === $value, "overwrite original tags = TRUE\n$frameID was:\n".var_dump_string($original_tags[$frameID])."\nis now:\n".var_dump_string($value)."\nin file: $tmpfile");
+    }
+    unset($t);
+    assert(copy($file, $tmpfile), "Couldn't copy $file to $tmpfile!");
+
+    // testing: overwrite original tags = false
+    $t = new TagLibMPEG($tmpfile);
+    $original_tags = $t->getID3v1();
+    $t->setID3v1(['TITLE'=>'testing'], false);
+    $new_tags = $t->getID3v1();
+    if($original_tags === false) {
+        assert($new_tags === false, "Tags were found in file $tmpfile after setID3v1 w/ overwrite original tags = FALSE!");
+    }
+    if(is_array($new_tags)) {
+        foreach($new_tags as $frameID => $value) {
+            assert($original_tags[$frameID] === $value, "overwrite original tags = FALSE\n$frameID was:\n".var_dump_string($original_tags[$frameID])."\nis now:\n".var_dump_string($value)."\nin file: $tmpfile");
+        }
+    }
+}
